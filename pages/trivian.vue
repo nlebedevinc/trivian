d8ca57e3283a9117ac2ee1e88b788e04f7057664
nlebedevinc <nlebedevme@gmail.com>
2023-11-17 14:36:08 -0500

    chore: change the info section

--- pages/trivian.vue
+++ pages/trivian.vue
@@ -51,10 +51,10 @@ const buildTime = useTimeAgo(config.public.buildTime as any)
 
       <div mt-15 flex="~ col gap-2">
         <div>
-          <span op50>Anthony Fu's </span> <span font-600 op75>QR</span><span font-400 op65> Toolkit</span>
+          <span op50>Nikolay Lebedev's </span> <span font-600 op75>Trivian</span>
         </div>
         <div flex="~ gap-1 items-center">
-          <span i-ri-arrow-right-line ml1 inline-block h-1em op50 /><a op75 hover:op100 href="https://antfu.me/posts/ai-qrcode-101" target="_blank">How to Generate AI QR Code</a><br>
+          <span i-ri-arrow-right-line ml1 inline-block h-1em op50 /><a op75 hover:op100 href="https://antfu.me/posts/ai-qrcode-101" target="_blank">What is Trivian?</a><br>
         </div>
         <div v-if="!hasParentWindow" flex="~ gap-1 items-center">
           <span i-ri-arrow-right-line ml1 inline-block h-1em op50 />
