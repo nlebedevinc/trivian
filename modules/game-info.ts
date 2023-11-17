53a02380bd3894e469bbe9a675d5ff1d010a2693
nlebedevinc <nlebedevme@gmail.com>
2023-11-16 20:42:39 -0500

    chore: meta

--- modules/game-info.ts
+++ modules/game-info.ts
@@ -3,6 +3,7 @@ import { game } from '../package.json'
 
 export default defineNuxtModule({
   setup(_, nuxt) {
+    // fill data scrutures with identifiers
     nuxt.options.appConfig.quiz = game.quiz
     nuxt.options.appConfig.meta = game.meta
     nuxt.options.appConfig.title = game.title
