4aa34e1b59de8038e86b84a3bb1df0bfe7275293
nlebedevinc <nlebedevme@gmail.com>
2023-12-19 15:04:53 -0800

    chore: switch

--- pages/qrcode.vue
+++ pages/qrcode.vue
@@ -6,26 +6,9 @@ const buildTime = useTimeAgo(config.public.buildTime as any)
 </script>
 
 <template>
-  <nav flex="~ gap4 none" border="b base" relative z-10 p4 bg-base>
-    <div flex-auto />
-    <h1 pointer-events-none absolute bottom-0 left-0 right-0 top-0 flex items-center justify-center text-xl font-light tracking-2px>
-      Trivian
-    </h1>
-    <div flex="~ gap-3 items-center">
-      <a href="https://github.com/nlebedevinc/trivian" target="_blank" text-lg op50 hover:op100>
-        <div i-ri-github-fill />
-      </a>
-      <a href="https://chat.antfu.me" target="_blank" text-lg op50 hover:op100>
-        <div i-ri-linkedin-fill />
-      </a>
-      <button op50 hover:op100 @click="toggleDark()">
-        <div i-ri-sun-fill dark:i-ri-moon-fill />
-      </button>
-    </div>
-  </nav>
-  <div flex="justify-center" p10 pb15 md:flex>
+  <div flex="~ justify-center" px3 py4 lg:p10 lg:py10>
     <div flex="~ col gap-4" class="max-w-full min-h-[calc(100vh-100px)] w-250">
-      <div fixed right-5 top-20 flex="col gap-2" hidden xl:flex>
+      <div fixed right-5 top-14 flex="col gap-2" hidden xl:flex>
         <VTooltip v-for="n in 10" :key="n" placement="left" distance="10">
           <button
             :style="{
@@ -45,16 +28,16 @@ const buildTime = useTimeAgo(config.public.buildTime as any)
         </VTooltip>
       </div>
 
-      <TStateProvider :key="storeIndex" :index="storeIndex" />
+      <StateProvider :key="storeIndex" :index="storeIndex" />
 
       <div flex-auto />
 
       <div mt-15 flex="~ col gap-2">
         <div>
-          <span op50>Nikolay Lebedev's </span> <span font-600 op75>Trivian</span>
+          <span op50>Anthony Fu's </span> <span font-600 op75>QR</span><span font-400 op65> Toolkit</span>
         </div>
         <div flex="~ gap-1 items-center">
-          <span i-ri-arrow-right-line ml1 inline-block h-1em op50 /><a op75 hover:op100 href="https://antfu.me/posts/ai-qrcode-101" target="_blank">What is Trivian?</a><br>
+          <span i-ri-arrow-right-line ml1 inline-block h-1em op50 /><a op75 hover:op100 href="https://antfu.me/posts/ai-qrcode-101" target="_blank">How to Generate AI QR Code</a><br>
         </div>
         <div v-if="!hasParentWindow" flex="~ gap-1 items-center">
           <span i-ri-arrow-right-line ml1 inline-block h-1em op50 />
@@ -62,12 +45,12 @@ const buildTime = useTimeAgo(config.public.buildTime as any)
             href="https://github.com/antfu/sd-webui-qrcode-toolkit"
             target="_blank"
             flex="~ inline gap-1 items-center" op75 hover:op100
-          >How to play?</a>
+          >Web UI Extension</a>
         </div>
         <div flex="~ gap-1 items-center">
           <span i-ri-arrow-right-line ml1 inline-block h-1em op50 />
           <a
-            href="https://github.com/nlebedevinc/trivian/issues"
+            href="https://github.com/antfu/qrcode-toolkit/issues"
             target="_blank"
             flex="~ inline gap-1 items-center" op75 hover:op100
           >Bug report / feature request</a>
@@ -99,13 +82,13 @@ const buildTime = useTimeAgo(config.public.buildTime as any)
           <div i-ri-github-fill />
         </a>
         <a href="https://chat.antfu.me" target="_blank" text-lg op50 hover:op100>
-          <div i-ri-linkedin-fill />
+          <div i-ri-discord-fill />
         </a>
         <a href="https://antfu.me" target="_blank" ml--0.5 text-lg op50 hover:op100>
           <img src="https://antfu.me/logo.svg" h-1.2em w-1.2em dark:invert>
         </a>
         <div flex="~ gap-1 items-center" ml-3>
-          <span op35>Made with </span><div text-red i-ri-heart-3-fill /><span op35> for </span><span font-bold op65></span><br>
+          <span op35>Made with </span> <a mt--2 href="https://nuxt.com" target="_blank" flex="~ inline gap-1 items-center" translate-y-0.9 op75 hover:op100><div i-logos-nuxt-icon /> <span font-bold op65>Nuxt</span></a><br>
         </div>
       </div>
     </div>
